4ca7600d045dcbb67bd25bc33fa38dda25cc7ef5
Jeremy <jeremymwatson@gmail.com>
2015-01-04 21:18:52 -0500

    initial commit

---
+++ newserver.js
@@ -0,0 +1,71 @@
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+  var ourl = 'http://thegradcafe.com/survey/index.php?q=brown&t=a&pp=250&o=&p=';
+
+  /*
+  * replace *a* with every letter: http://thegradcafe.com/survey/index.php?q=*a*&t=a&o=&pp=250
+  *
+  *
+  *
+  */
+
+  var school, program, decision, greQ, greV, greW, GPA;
+  var id_obj = { idNo : "" , school : "", program : "", decision : "", season : "",
+      student_type : "", greV : "", greQ : "", greW : "", greSub : "", GPA : ""};
+
+  var pages = 14;
+  var page_index;
+  for (page_index = 1; page_index <= pages; page_index++) {
+
+    url = ourl + page_index;
+    console.log(url);
+
+    request(url, function(error, response, html){
+
+      if (error)
+        console.log(error);
+      else {
+
+        var $ = cheerio.load(html);
+
+        var tr_array = $('[class^="row"]');
+
+        // Remove any rows none relevant rows
+        tr_array = tr_array.filter(function (rowF) {
+          return !('undefined' === typeof(tr_array[rowF].children[2].children[2]) || typeof(tr_array[rowF].children[2].children[0]) === 'undefined');
+        });
+
+        var index, len;
+        for (index = 0, len = tr_array.length; index < len; ++index) {
+
+          id_obj.school = tr_array[index].children[0].children[0].data; //University
+          id_obj.program = tr_array[index].children[1].children[0].data.substring(0, tr_array[index].children[1].children[0].data.length - 6); //program etc
+          id_obj.decision = tr_array[index].children[2].children[0].children[0].data; //accepted/rejected
+
+          if (typeof(tr_array[index].children[3].children[0]) === 'undefined')
+            id_obj.student_type = "";
+          else
+            id_obj.student_type = tr_array[index].children[3].children[0].data; //type of student
+
+          id_obj.GPA = tr_array[index].children[2].children[2].children[0].children[1].data.substr(2);  // GPA
+
+          var GREs = (tr_array[index].children[2].children[2].children[0].children[4].data).split("/"); // GRE
+          id_obj.greV = GREs[0].replace(/\D/g,'');
+          id_obj.greQ = GREs[1].replace(/\D/g,'');
+          id_obj.greW = GREs[2];
+
+          id_obj.greSub = tr_array[index].children[2].children[2].children[0].children[7].data.substr(2); // GRE subject
+
+          id_obj.season = tr_array[index].children[1].children[0].data.slice(-5).replace(/[()]/g,'');
+
+
+
+          id_obj.idNo = (tr_array[index].attribs.onmouseover).replace(/\D/g,'');
+
+          console.log(JSON.stringify(id_obj));
+        }
+      }
+    })
+  }
--- server.js
+++ server.js
@@ -2,39 +2,60 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-
-  // The URL we will scrape from - in our example Anchorman 2.
-
+  var listO = new Array();
   var url = 'http://thegradcafe.com/survey/index.php?q=brown';
-  //var json_array;
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 parameters, an error, response status code and the html
 
-    var school, program, decision, greQ, greV, greW, GPA;
-    var json = { school : "", program : "", decision : "", greV : "", greQ : "", greW : "", GPA : ""};
+  /*
+  * replace *a* with every letter: http://thegradcafe.com/survey/index.php?q=*a*&t=a&o=&pp=250
+  *
+  *
+  *
+  */
 
-  request(url, function(error, response, html){
+  var school, program, decision, greQ, greV, greW, GPA;
+  var id_obj = { idNo : "" , school : "", program : "", decision : "", season : "", year : "" ,
+      student_type : "", greV : "", greQ : "", greW : "", greSub : "", GPA : ""};
 
-    // First we'll check to make sure no errors occurred when making the request
-    console.log(error);
-    if(!error){
-      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-      var $ = cheerio.load(html);
-      // Finally, we'll define the variables we're going to capture
-      var tr_array = $('[class^="row"]');
-      //var test = $.getElementByClassName('[class^="row"]');
-    //  console.log(tr_array.children('td.instcol').text());
-      console.log(tr_array[0].children[1]);
-    //  tr_array.forEach
+  var pages = 14;
+  var page_index;
+  for (page_index = 1; index <= pages; page_index++) {
 
 
-    //  $('table.results').each(function(i,element){
-    //    var results_table = $(this).children().next();
-    //    console.log(results_table('').text());
-    //  });
+    request(url, function(error, response, html){
 
+      // First we'll check to make sure no errors occurred when making the request
+      if (error)
+        console.log(error);
+      if(!error){
+        // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
 
-    }
-  })
+        var $ = cheerio.load(html);
+
+        var tr_array = $('[class^="row"]');
+
+        var index, len;
+        var a = ["a", "b", "c"];
+        for (index = 0, len = a.length; index < len; ++index) {
+          console.log(a[index]);
+        }
+        
+          id_obj.school = tr_array[5].children[0].children[0].data; //University
+          id_obj.program = tr_array[5].children[1].children[0].data; //program etc
+          id_obj.decision = tr_array[5].children[2].children[0].children[0].data; //accepted/rejected
+          id_obj.student_type = tr_array[5].children[3].children[0].data; //type of student
+
+          id_obj.GPA = tr_array[5].children[2].children[2].children[0].children[1].data.substr(2);  // GPA
+
+          var GREs = (tr_array[5].children[2].children[2].children[0].children[4].data).split("/"); // GRE
+          id_obj.greV = GREs[0];
+          id_obj.greQ = GREs[1];
+          id_obj.greW = GREs[2];
+
+          id_obj.greSub = tr_array[5].children[2].children[2].children[0].children[7].data.substr(2); // GRE subject, test for ": n/a"
+          id_obj.idNo = (tr_array[5].attribs.onmouseover).replace(/\D/g,'');
+
+          listO.push(id_obj);
+
+      }
+    })
+  }
